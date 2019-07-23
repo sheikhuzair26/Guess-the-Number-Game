90b3c7db2392f9c5e2db148fb49bf625c5d9e019
Uzair Javed <uzairjaved75@gmail.com>
2019-07-23 17:43:17 +0500

    Add files via upload

--- guessCalculator.js
+++ guessCalculator.js
@@ -1,32 +1,48 @@
+let wrongGuessNumber = 0;
+
+function general() {
+    document.querySelector("#mainSec").style = "display : none;";
+    document.querySelector("footer").style = "display : none;";
+    document.querySelector("h1").style = "display : none;";
+    document.querySelector("h2").style = "display : none;";
+    document.querySelector("h3").style = "display : flex;";
+
+}
+
+function backFunction() {
+    window.location.reload();
+}
+
 
 function easyGame() {
+    general();
     document.querySelector("#easyGuess").style = "display : flex;";
     document.querySelector("#easyButton").style = "display : flex;";
-    document.querySelector("#mainSec").style = "display : none;";
-    document.querySelector("footer").style = "display : none;";
-    document.querySelector("h1").style = "display : none;";
-    document.querySelector("h2").style = "display : none;";
-    document.querySelector("h3").style = "display : flex;";
+    document.querySelector("#firstPopSec p").style = "display : flex;";
+    document.querySelector("#secondPopSec p").style = "display : none;";
+    document.querySelector("#thirdPopSec p").style = "display : none;";
+    document.querySelector("#easyBack").style = "display : flex;";
     document.body.style.backgroundImage = "url('pop2.jpg')";
+
 }
 function mediumGame() {
+    general();
     document.querySelector("#mediumGuess").style = "display : flex;";
     document.querySelector("#mediumButton").style = "display : flex;";
-    document.querySelector("#mainSec").style = "display : none;";
-    document.querySelector("footer").style = "display : none;";
-    document.querySelector("h1").style = "display : none;";
-    document.querySelector("h2").style = "display : none;";
-    document.querySelector("h3").style = "display : flex;";
+    document.querySelector("#firstPopSec p").style = "display : none;";
+    document.querySelector("#secondPopSec p").style = "display : flex;";
+    document.querySelector("#thirdPopSec p").style = "display : none;";
+    document.querySelector("#mediumBack").style = "display : flex;"
     document.body.style.backgroundImage = "url('amazing.jpg')";
 }
 function hardGame() {
+    general();
     document.querySelector("#hardGuess").style = "display : flex;";
     document.querySelector("#hardButton").style = "display : flex;";
-    document.querySelector("#mainSec").style = "display : none;";
-    document.querySelector("footer").style = "display : none;";
-    document.querySelector("h1").style = "display : none;";
-    document.querySelector("h2").style = "display : none;";
-    document.querySelector("h3").style = "display : flex;";
+    document.querySelector("#secondPopSec p").style = "display : none;";
+    document.querySelector("#firstPopSec p").style = "display : none;";
+    document.querySelector("#thirdPopSec p").style = "display : flex;";
+    document.querySelector("#hardBack").style = "display : flex;"
     document.body.style.backgroundImage = "url('aside.jpg')";
 }
 
@@ -37,58 +53,65 @@ let randomIndexHard = parseInt(Math.random() * 1000);
 
 function easyFunction() {
 
-    let name = document.querySelector("#easyGuess").value;
-
+    let name = parseInt(document.querySelector("#easyGuess").value);
     console.log(randomIndexEasy);
 
-    if (name == randomIndexEasy) {
-        alert("CongRatulations You Won ");
+    if (name === randomIndexEasy) {
+        alert("CongRatulations You Won! Your Wrong Guess are " + wrongGuessNumber);
+        wrongGuessNumber = 0;           //Random number Generate Again
     }
     else {
         if (name > randomIndexEasy) {
             alert("Too Big Value ");
+            wrongGuessNumber++;     //Attempts wrong
+
         }
         else if (name < randomIndexEasy) {
             alert("You Are So Far Keep Going");
+            wrongGuessNumber++;
         };
     };
 };
 
-
 function mediumFunction() {
 
-    let name = document.querySelector("#mediumGuess").value;
-
+    let name = parseInt(document.querySelector("#mediumGuess").value);
     console.log(randomIndexMedium);
 
-    if (name == randomIndexMedium) {
-        alert("CongRatulations You Won Medium Battle ");
+    if (name === randomIndexMedium) {
+        alert("CongRatulations You Won Medium Battle! Your Wrong Guess are " + wrongGuessNumber);
+        wrongGuessNumber = 0;
     }
     else {
         if (name > randomIndexMedium) {
             alert("Too big VAlue");
+            wrongGuessNumber++;
         }
         else if (name < randomIndexMedium) {
             alert("You are so Far Keep Going");
+            wrongGuessNumber++;
         };
     };
 };
 
 function hardFunction() {
 
-    let name = document.querySelector("#hardGuess").value;
-
+    let name = parseInt(document.querySelector("#hardGuess").value);
     console.log(randomIndexHard);
 
-    if (name == randomIndexHard) {
-        alert("CongRatulations you have Won Hardest Battle");
+    if (name === randomIndexHard) {
+        alert("CongRatulations you have Won Hardest Battle! Your Wrong Guess are " + wrongGuessNumber);
+        wrongGuessNumber = 0;
+
     }
     else {
         if (name > randomIndexHard) {
             alert("too big VAlue");
+            wrongGuessNumber++;
         }
         else if (name < randomIndexHard) {
             alert("You are So Far Keep Going");
+            wrongGuessNumber++;
         };
     };
 };
